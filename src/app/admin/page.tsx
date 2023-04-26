aae74ac5d5ee0bf91bb400b8db44fd320adfe4fc
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-04-27 02:38:40 +0700

    feat: role access

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { headers, cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import Logout from "./components/Logout";
 
 export default async function Page() {
 	const supabase = createServerComponentSupabaseClient({
@@ -16,20 +15,20 @@ export default async function Page() {
 		return redirect("/auth");
 	}
 
+	const { data: user } = await supabase.from("user").select("*").single();
+
+	if (user?.role !== "admin") {
+		return redirect("/");
+	}
+
 	const { data: post } = await supabase.from("post").select("*");
+
 	return (
-		<>
-			<div className="flex justify-center items-center h-96 flex-col gap-5">
-				<h1 className="text-5xl">Next.js 13 + Supabase</h1>
-				<h1 className="text-xl">Authentication</h1>
-				<h1 className="text-xl">Data Fetching with policy</h1>
-				<h1 className="text-xl">Page protection</h1>
-				<h1 className="text-xl text-white bg-green-500">
-					Role base access and data fetching
-				</h1>
-				<pre>{JSON.stringify(post, null, 2)}</pre>
-			</div>
-			<Logout />
-		</>
+		<div>
+			<h1 className="text-3xl font-bold">Admin role access only</h1>
+			<pre>{JSON.stringify(user, null, 2)}</pre>
+
+			<pre>{JSON.stringify(post, null, 2)}</pre>
+		</div>
 	);
 }
--- src/app/components/Auth.tsx
+++ src/app/components/Auth.tsx
@@ -5,8 +5,22 @@ import { useSupabase } from "../supabase-provider";
 export default function Auth() {
 	const { supabase } = useSupabase();
 
+	const handleCreateUser = async () => {
+		const { data, error } = await supabase.auth.signUp({
+			email: "example@email.com",
+			password: "example-password",
+		});
+	};
+
+	const handleLogin = async () => {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email: "example@email.com",
+			password: "example-password",
+		});
+	};
+
 	return (
-		<div className="h-screen flex justify-center items-center">
+		<div className="h-screen flex justify-center items-center flex-col">
 			<button
 				className="bg-black text-white px-5 py-3 rounded-md"
 				onClick={() =>
@@ -15,6 +29,8 @@ export default function Auth() {
 			>
 				Login with Github
 			</button>
+			<button onClick={handleCreateUser}>Sign Up new User</button>
+			<button onClick={handleLogin}>Sign IN new User</button>
 		</div>
 	);
 }
